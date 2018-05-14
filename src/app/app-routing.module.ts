3e30a711e6dc7b72f69e6099e955de893ea77baa
jacky860226 <jacky860226@yahoo.com.tw>
2018-05-14 15:13:22 +0800

    using ng-bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
  
 import { MainPageComponent }   from './main-page/main-page.component';
- 
+import { ProblemListComponent }   from './problem-list/problem-list.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/skyOJ', pathMatch: 'full' },
+  { path: 'problem', component: ProblemListComponent },
   { path: 'skyOJ', component: MainPageComponent }
 ];
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProblemListComponent } from './problem-list/problem-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MainPageComponent
+    MainPageComponent,
+    ProblemListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
