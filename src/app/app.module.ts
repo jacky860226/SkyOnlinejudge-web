24323d2148ea6fb074a3250d1a99fb698e38071f
jacky860226 <jacky860226@yahoo.com.tw>
2018-05-17 09:55:33 +0800

    auto merge problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { HttpClientModule }    from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -11,7 +9,6 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ProblemListComponent } from './problem-list/problem-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { ImageBarComponent } from './image-bar/image-bar.component';
 import { ChallengeListComponent } from './challenge-list/challenge-list.component';
