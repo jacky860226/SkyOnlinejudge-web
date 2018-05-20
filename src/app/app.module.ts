f624c10e0fab5c2a949002aef32701722198d9b9
Kaiwen Sz <kaiwen0701@gmail.com>
2018-05-21 02:50:06 +0800

    支援 list.service 得到要顯示的資料，並用 in-memory-data.service 來實現假後端。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -11,7 +10,6 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ProblemListComponent } from './problem-list/problem-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { ImageBarComponent } from './image-bar/image-bar.component';
 import { ChallengeListComponent } from './challenge-list/challenge-list.component';
@@ -19,6 +17,8 @@ import { ChallengeListComponent } from './challenge-list/challenge-list.componen
 import { HttpClientModule }    from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import { ListComponent } from './list/list.component';
+import { ContestListComponent } from './contest-list/contest-list.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,9 @@ import { InMemoryDataService }  from './in-memory-data.service';
     ImageBarComponent,
     ChallengeListComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    ListComponent,
+    ContestListComponent
   ],
   imports: [
     BrowserModule,
